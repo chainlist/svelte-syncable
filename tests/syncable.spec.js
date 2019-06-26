96aae87144654b4c6f273406c130a9364a1ba18d
Kevin Guillouard <kevin.guillouard@gmail.com>
2019-06-26 11:00:18 +0200

    Add new tests

--- tests/syncable.spec.js
+++ tests/syncable.spec.js
@@ -1,5 +1,6 @@
 import App from './App.svelte';
 import { syncable, setPrefix } from '../index';
+import { get } from 'svelte/store';
 
 let app = null;
 
@@ -10,12 +11,12 @@ describe('syncable', () => {
     localStorage.clear();
   });
   
-  test('App is OKAY', () => {
+  test('App is constructed', () => {
     app = new App({ target: document.body });
     expect(app).toBeTruthy();
   });
 
-  test('syncable store in localStorage', () => {
+  test('syncable stores in localStorage', () => {
     syncable('count', 0);
 
     expect(localStorage.setItem).toHaveBeenLastCalledWith('svelteStore-count', "0");
@@ -28,7 +29,7 @@ describe('syncable', () => {
     expect(localStorage.setItem).toHaveBeenLastCalledWith('foobar-count', "0");
   });
 
-  test('syncable return a SvelteObservable', () => {
+  test('syncable returns a SvelteObservable', () => {
     const count = syncable('count', 0);
 
     expect(count.subscribe).toBeInstanceOf(Function);
@@ -36,7 +37,7 @@ describe('syncable', () => {
     expect(count.update).toBeInstanceOf(Function);
   });
 
-  test('update observable from a component', () => {
+  test('stores is synced on component actions', () => {
     let app = new App({ target: document.body });
 
     const count = document.querySelector('#count');
@@ -55,5 +56,21 @@ describe('syncable', () => {
     decrement.click();
     expect(localStorage.getItem('svelteStore-count')).toBe('0');
     setTimeout(() => expect(count.innerText).toBe('0'));
+  });
+
+  test('syncable is hydrated from the stored value', () => {
+    localStorage.setItem('svelteStore-count', 42);
+    
+    const countObservable = syncable('count', 0);
+
+    expect(get(countObservable)).toBe(42);
+  });
+
+  test('syncable is not hydrated from the stored value', () => {
+    localStorage.setItem('svelteStore-count', 42);
+
+    const countObservable = syncable('count', 0, false);
+
+    expect(get(countObservable)).toBe(0);
   });
 });
